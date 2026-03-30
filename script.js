eea4028bd42fda70a20c3492ff8720305cf875dd
Vedant Jadhav <121371485+vedant2863@users.noreply.github.com>
2026-03-30 08:45:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 //your JS code here. If required.
+const form = document.getElementById("form");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const name = document.getElementById("name").value.trim();
+  const age = document.getElementById("age").value.trim();
+
+  // ✅ Validation
+  if (name === "" || age === "") {
+    alert("Please enter valid details.");
+    return;
+  }
+
+  // ✅ Promise
+  const checkAge = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (Number(age) > 18) {
+        resolve(`Welcome, ${name}. You can vote.`);
+      } else {
+        reject(`Oh sorry ${name}. You aren't old enough.`);
+      }
+    }, 4000);
+  });
+
+  // ✅ Handling Promise
+  checkAge
+    .then((message) => {
+      alert(message);
+    })
+    .catch((error) => {
+      alert(error);
+    });
+});
